21e8c68d1d9f8d98e54b08cb286b6f84ca5b2cad
Ilia Jackson <roxnwine@gmail.com>
2016-10-27 22:24:16 -0700

    first commit

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var lyricsView: UITextView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func reset(_ sender: AnyObject) {
+        nameField.text = ""
+        lyricsView.text = ""
     }
 
+    @IBAction func displayLyrics(_ sender: AnyObject) {
+        if let nameFieldText = nameField.text {
+            lyricsView.text = lyricsForName(name: nameFieldText)
+        }
+    }
+        }
 
+func shortNameFromName(name: String) -> String{
+    
+    let lowercaseName = name.lowercased()
+    let vowelSet = NSCharacterSet(charactersIn: "aeiou")
+   // if let firstVowelRange = lowercaseName.rangeOfCharacterFromSet(vowelSet, options: .CaseInsensitiveSearch){
+    
+     if let firstVowelRange = lowercaseName.rangeOfCharacter(from: vowelSet as CharacterSet) {
+        return lowercaseName.substring(from: firstVowelRange.lowerBound)
+    }
+    return lowercaseName
 }
 
+
+func lyricsForName(name: String) -> String {
+    let short = shortNameFromName(name: name)
+    
+    return "#f, #f, Bo B#s\nBanana Fana Fo F#s\nMe My Mo M#s\n#f"
+
+        .replacingOccurrences(of: "#f", with: name)
+        .replacingOccurrences(of: "#s", with: short)
+}
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ nameField: UITextField) -> Bool {
+        nameField.resignFirstResponder()
+        return false
+    }
+}
